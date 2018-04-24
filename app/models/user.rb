00da2dd4dbdb6d9b2c548b76ae19a3b38deb08f2
Claudio Russi <claudiomrussi@hotmail.com>
2018-04-24 01:32:54 -0300

    Add model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :subjects
+  has_many :tasks, through: :subjects
 end
